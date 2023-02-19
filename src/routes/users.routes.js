576a521d9c644e838aee9ea0448865b9e9ff1ca1
Evander Inacio <evander.20112@hotmail.com>
2023-02-19 15:21:59 -0300

    Feat: Error :goal_net:

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,15 @@ const UsersController = require("../controllers/UsersController")
 
 const UsersRoutes = Router()
 
+// function to check the content of the request
+// function myMiddleware(request, response, next) {
+//   console.log("voce passou pelo middleware")
+
+//   next() // call the function to be executed (usersController)
+// }
+
+
+
 const usersController = new UsersController()
  
 UsersRoutes.post("/", usersController.create)
