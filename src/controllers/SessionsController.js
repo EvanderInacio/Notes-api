4df3547b5c6bb54eee3049a1f71a1de47f77ebd2
Evander Inacio <evander.20112@hotmail.com>
2023-05-27 20:05:56 -0300

    Feat: Token creation and user authentication :dizzy:

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -3,6 +3,9 @@ const knex = require('../database/knex');
 const AppError = require('../utils/AppError')
 const { compare } = require("bcryptjs")
 
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body
@@ -18,8 +21,14 @@ class SessionsController {
     if(!passwordMatched) {
       throw new AppError("E-mail e/ou senha incorreta", 401)
     }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
     
-    return response.json(user)
+    return response.json({ user, token })
   }
 }
 
